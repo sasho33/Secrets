d0c6fcb4d89acdd8c466bcc3af0b461a54450b56
sasho33 <sasho3333333@gmail.com>
2022-09-11 23:58:40 +0100

    md5 encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+var md5 = require('md5');
 
 const app = express();
 
@@ -24,11 +24,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-});
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,7 +31,7 @@ const User = new mongoose.model("User", userSchema);
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (!err) {
@@ -52,7 +47,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({
     email: username
